13087147dae31b41f543fda4147d29e8e3d868e6
Dilshan Madushanka <helloscoopz@gmail.com>
2024-09-13 05:03:57 +0530

    Update existing tests

--- src/run-cache.test.ts
+++ src/run-cache.test.ts
@@ -7,17 +7,23 @@ describe("RunCache", () => {
 
   describe("set()", () => {
     it("should throw an error if the cache key or value is empty", () => {
-      expect(() => RunCache.set("", "value1")).toThrow("Empty key");
-      expect(() => RunCache.set("key1", "")).toThrow("Empty value");
+      expect(() => RunCache.set({ key: "", value: "value1" })).toThrow(
+        "Empty key",
+      );
+      expect(() => RunCache.set({ key: "key1", value: "" })).toThrow(
+        "Empty value",
+      );
     });
 
     it("should return true if the cache value set successfully", () => {
-      expect(RunCache.set("key1", "value1")).toBe(true);
+      expect(RunCache.set({ key: "key1", value: "value1" })).toBe(true);
     });
 
-    it("should return true if the cache set with a ttl and ttl is functioning properly", () => {
-      expect(RunCache.set("key2", "value2", 100)).toBe(true);
-      expect(RunCache.get("key2")).toBe("value2");
+    it("should return true if the cache set with a ttl and ttl is functioning properly", async () => {
+      expect(RunCache.set({ key: "key2", value: "value2", ttl: 100 })).toBe(
+        true,
+      );
+      expect(await RunCache.get("key2")).toBe("value2");
 
       // Wait for the TTL to expire
       setTimeout(() => {
@@ -27,17 +33,17 @@ describe("RunCache", () => {
   });
 
   describe("get()", () => {
-    it("should return undefined if the key is empty", () => {
-      expect(RunCache.get("")).toBeUndefined();
+    it("should return undefined if the key is empty", async () => {
+      expect(await RunCache.get("")).toBeUndefined();
     });
 
-    it("should return undefined if the key is not found", () => {
-      expect(RunCache.get("key1")).toBeUndefined();
+    it("should return undefined if the key is not found", async () => {
+      expect(await RunCache.get("key1")).toBeUndefined();
     });
 
-    it("should return the value successfully", () => {
-      RunCache.set("key1", "value1");
-      expect(RunCache.get("key1")).toBe("value1");
+    it("should return the value successfully", async () => {
+      RunCache.set({ key: "key1", value: "value1" });
+      expect(await RunCache.get("key1")).toBe("value1");
     });
   });
 
@@ -46,26 +52,26 @@ describe("RunCache", () => {
       expect(RunCache.delete("nonExistentKey")).toBe(false);
     });
 
-    it("should return true if the value is successfully deleted", () => {
-      RunCache.set("key1", "value1");
+    it("should return true if the value is successfully deleted", async () => {
+      RunCache.set({ key: "key1", value: "value1" });
       expect(RunCache.delete("key1")).toBe(true);
-      expect(RunCache.get("key1")).toBeUndefined();
+      expect(await RunCache.get("key1")).toBeUndefined();
     });
   });
 
   describe("deleteAll()", () => {
-    it("should clear all values", () => {
-      RunCache.set("key1", "value1");
-      RunCache.set("key2", "value2");
+    it("should clear all values", async () => {
+      RunCache.set({ key: "key1", value: "value1" });
+      RunCache.set({ key: "key2", value: "value2" });
       RunCache.deleteAll();
-      expect(RunCache.get("key1")).toBeUndefined();
-      expect(RunCache.get("key2")).toBeUndefined();
+      expect(await RunCache.get("key1")).toBeUndefined();
+      expect(await RunCache.get("key2")).toBeUndefined();
     });
   });
 
   describe("has()", () => {
     it("should return true if the key exists", () => {
-      RunCache.set("key1", "value1");
+      RunCache.set({ key: "key1", value: "value1" });
       expect(RunCache.has("key1")).toBe(true);
     });
 
@@ -74,7 +80,7 @@ describe("RunCache", () => {
     });
 
     it("should return false after ttl expiry", () => {
-      RunCache.set("key2", "value2", 50); // Set TTL to 50ms
+      RunCache.set({ key: "key2", value: "value2", ttl: 50 }); // Set TTL to 50ms
       expect(RunCache.has("key2")).toBe(true);
 
       // Wait for the TTL to expire
@@ -89,21 +95,27 @@ describe("RunCache", () => {
       let sourceFn = async () => {
         throw Error("Unexpected Error");
       };
-      expect(RunCache.setWithSourceFn("key1", sourceFn)).rejects.toThrow(
-        "Source function failed",
-      );
+      expect(
+        RunCache.setWithSourceFn({
+          key: "key1",
+          sourceFn,
+        }),
+      ).rejects.toThrow("Source function failed");
     });
 
     it("should be able to set a value with source function successfully", async () => {
       const sourceFn = async () => "dynamicValue";
-      await RunCache.setWithSourceFn("key2", sourceFn);
-      expect(RunCache.get("key2")).toBe('"dynamicValue"');
+      await RunCache.setWithSourceFn({
+        key: "key2",
+        sourceFn,
+      });
+      expect(await RunCache.get("key2")).toBe('"dynamicValue"');
     });
   });
 
   describe("refetch()", () => {
     it("should throw an error if refetch is called on a key having no source function", async () => {
-      RunCache.set("key2", "value2");
+      RunCache.set({ key: "key2", value: "value2" });
       await expect(RunCache.refetch("key2")).rejects.toThrow(
         "No source function found",
       );
@@ -119,7 +131,7 @@ describe("RunCache", () => {
           return "SomeValue";
         }
       };
-      await RunCache.setWithSourceFn("key3", sourceFn);
+      await RunCache.setWithSourceFn({ key: "key3", sourceFn });
 
       // Make source function to fail
       breaker = true;
@@ -137,14 +149,14 @@ describe("RunCache", () => {
       let dynamicValue = "initialValue";
       const sourceFn = async () => dynamicValue;
 
-      await RunCache.setWithSourceFn("key1", sourceFn);
-      expect(RunCache.get("key1")).toBe('"initialValue"');
+      await RunCache.setWithSourceFn({ key: "key1", sourceFn });
+      expect(await RunCache.get("key1")).toBe('"initialValue"');
 
       // Update what's being returned in the source function
       dynamicValue = "updatedValue";
 
       await RunCache.refetch("key1");
-      expect(RunCache.get("key1")).toBe('"updatedValue"');
+      expect(await RunCache.get("key1")).toBe('"updatedValue"');
     });
   });
 });
